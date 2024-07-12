a4c264e957e26c4c7c3b247da6db7cdd896b0105
Siddharth Malik <siddmalik0418@gmail.com>
2024-07-12 08:54:20 +0530

    Add main changes

--- app/src/main/java/com/example/multilingual/MainActivity.kt
+++ app/src/main/java/com/example/multilingual/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.example.multilingual
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var inptxt:EditText
+    lateinit var chngbtn:Button
+    lateinit var srchlang:SearchView
+    lateinit var listlang:ListView
+    lateinit var inpTxt:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        initall()
+        inpTxt=inptxt.text.toString()
+        chngbtn.setOnClickListener(){
+            if (inpTxt.isEmpty()) {
+                Toast.makeText(this,"Please enter text", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
         }
+
+    }
+
+    fun initall(){
+        inptxt=findViewById(R.id.inptxt)
+        chngbtn=findViewById(R.id.chngbtn)
+        srchlang=findViewById(R.id.srchlang)
+        listlang=findViewById(R.id.listlang)
     }
 }
