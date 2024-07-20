8de38e1ea1f51ba19ff2d74d9574d9837e979d71
Siddharth Malik <siddmalik0418@gmail.com>
2024-07-20 17:22:24 +0530

    Add all fields

--- app/src/main/java/com/example/multilingual/MainActivity.kt
+++ app/src/main/java/com/example/multilingual/MainActivity.kt
@@ -1,65 +1,40 @@
 package com.example.multilingual
 
-import android.graphics.Color
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Adapter
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ListView
-import android.widget.SearchView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.multilingual.R.layout.itemstyle
-import com.example.multilingual.databinding.ActivityMainBinding
-import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
-    lateinit var inptxt:EditText
-    lateinit var chngbtn:Button
-    lateinit var srchlang:SearchView
-    lateinit var listlang:ListView
-    lateinit var langlist:ArrayList<langformat>
-    lateinit var langitems:ArrayList<Langitems>
-    private lateinit var binding:ActivityMainBinding
+    lateinit var inptxt: EditText
+    lateinit var chngbtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        binding=ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
         initall()
-        loadalllang()
 
-
-    }
-
-    fun initall(){
-        inptxt=findViewById(R.id.inptxt)
-        chngbtn=findViewById(R.id.chngbtn)
-        srchlang=findViewById(R.id.srchlang)
-        listlang=findViewById(R.id.listlang)
-    }
-    fun loadalllang(){
-        langlist=ArrayList()
-        val langcodels= com.google.mlkit.nl.translate.TranslateLanguage.getAllLanguages()
-        for(code in langcodels){
-            val langtitle= Locale(code).displayLanguage
-
-            val langfor=langformat(code,langtitle)
-            langlist.add(langfor)
-            langitems= ArrayList()
-            for (i in langlist.indices){
-                var langi=Langitems(langlist[i].langtitle)
-                langitems.add(langi)
+        chngbtn.setOnClickListener {
+            val inpTxt = inptxt.text.toString()
+            if (inpTxt.isEmpty()) {
+                Toast.makeText(this, "Please enter text", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
             }
-            binding.listlang.adapter=Adapter(this,langitems)
+            val intent = Intent(this, MainActivity2::class.java).apply {
+                putExtra("inptxt", inpTxt)
+            }
+            startActivity(intent)
         }
+    }
+
+    private fun initall() {
+        inptxt = findViewById(R.id.inptxt)
+        chngbtn = findViewById(R.id.chngbtn)
     }
 }
